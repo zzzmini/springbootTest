eb19c088780d328dd2d78db358945f1879cf699d
zzzmini <sm01079200887@gmail.com>
2024-03-23 08:43:49 +0900

    스프링부트 작업 준비 진행 1. 깃 연결 2. .gitIgnore 설정

---
+++ src/main/java/com/myboard/controller/MyFirstController.java
@@ -0,0 +1,12 @@
+package com.myboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyFirstController {
+    @GetMapping("/hi")
+    public String hi(){
+        return "greetings";
+    }
+}

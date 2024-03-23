6805b3f89a3d8272a7fc5854d08e9445d8e85464
zzzmini <sm01079200887@gmail.com>
2024-03-23 11:04:01 +0900

    #3 - bootstrap 적용

--- src/main/java/com/myboard/controller/MyFirstController.java
+++ src/main/java/com/myboard/controller/MyFirstController.java
@@ -1,6 +1,7 @@
 package com.myboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,7 +12,8 @@ public class MyFirstController {
     }
 
     @GetMapping("/test")
-    public String test(){
+    public String test(Model model){
+        model.addAttribute("name", "김형민");
         return "test";
     }
 }

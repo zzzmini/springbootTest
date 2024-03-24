8bab37d79de908dd0464860ec91b43d6def41c40
zzzmini <sm01079200887@gmail.com>
2024-03-24 15:32:36 +0900

    #9-전체 데이터 검색해서 출력하기

--- src/main/java/com/myboard/controller/ArticleController.java
+++ src/main/java/com/myboard/controller/ArticleController.java
@@ -1,14 +1,21 @@
 package com.myboard.controller;
 
+import com.myboard.dto.ArticleForm;
 import com.myboard.dto.ArticleForm_Old;
 import com.myboard.entity.Article;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.myboard.repository.ArticleRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j   // 로깅을 위한 추가
 @Controller
 public class ArticleController {
@@ -31,5 +38,38 @@ public class ArticleController {
 //        System.out.println(saved);
         log.info(saved.toString());
         return "/result";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String showOneArticle(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //log.info(articleEntity.toString());
+        ArticleForm dto = ArticleForm.to(articleEntity);
+        log.info(dto.toString());
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("dto", dto);
+        // 3. 뷰  페이지 반환하기
+        return "/articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String showAllArticles(Model model){
+        // 1. 모든 데이터 가져오기
+        List<Article> articleList = new ArrayList<>();
+        List<ArticleForm> articleFormList = new ArrayList<>();
+        ArticleForm form;
+        articleList = articleRepository.findAll();
+        for(Article article : articleList){
+            form = ArticleForm.to(article);
+            articleFormList.add(form);
+        }
+
+        // 2. dtoList 에 담기
+        model.addAttribute("articles", articleFormList);
+        // 3. 뷰페이지에 전달하기
+        return "/articles/show_all";
     }
 }
--- src/main/java/com/myboard/dto/ArticleForm.java
+++ src/main/java/com/myboard/dto/ArticleForm.java
@@ -11,10 +11,19 @@ import lombok.ToString;
 @Getter
 @Setter
 public class ArticleForm {
+    private Long id;
     private String title;  // 제목을 받을 필드
     private String content;  // 내용을 받을 필드
 
     public Article toEntity(){
         return new Article(null, title, content);
+    }
+
+    public static ArticleForm to(Article article){
+        return new ArticleForm(
+                article.getId(),
+                article.getTitle(),
+                article.getContent()
+                );
     }
 }
